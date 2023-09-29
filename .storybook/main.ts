9473232c9f978bd86333a0a93e2e4a150bed6dfe
Matheus Iam <matheusiam27@gmail.com>
2023-09-29 11:08:50 -0400

    adidionado storybook

---
+++ .storybook/main.ts
@@ -0,0 +1,16 @@
+const config = {
+  stories: ['../src/components/**/stories.tsx'],
+  addons: ['@storybook/addon-essentials'],
+  framework: {
+    name: '@storybook/nextjs',
+    options: {}
+  },
+  docs: {
+    autodocs: true
+  },
+  webpackFinal: (config) => {
+    config.resolve.modules.push(`${process.cwd()}/src`)
+    return config
+  }
+}
+export default config
