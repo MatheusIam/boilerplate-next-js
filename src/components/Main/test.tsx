61fa243cfa948ceb21a7cafea9c7f804dcf44a01
Matheus Iam <matheusiam27@gmail.com>
2023-09-29 09:19:23 -0400

    configurado jest-styled-components

--- src/components/Main/test.tsx
+++ src/components/Main/test.tsx
@@ -1,5 +1,4 @@
 import { render, screen } from '@testing-library/react'
-
 import Main from '.'
 
 describe('<Main />', () => {
@@ -14,5 +13,13 @@ describe('<Main />', () => {
 
     // gerar snapshot
     expect(container.firstChild).toMatchSnapshot()
+  })
+
+  it('should render the colors correctly', () => {
+    // renderiza o component
+    const { container } = render(<Main />)
+
+    // verifica se o background-color está correto
+    expect(container.firstChild).toHaveStyle({ 'background-color': '#06092b' })
   })
 })
